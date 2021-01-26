5ed5e723a3635a50a84cc6fcbeffe6dbd5be2b87
superior-prog <meherajmahmmd@gmail.com>
2021-01-26 22:47:43 +0600

    Update models.py

--- entries/models.py
+++ entries/models.py
@@ -1,10 +1,11 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 from users.models import User
 
 
 class Entry(models.Model):
     entry_title = models.CharField(max_length=255)
-    entry_text = models.TextField()
+    entry_text = RichTextField()
     entry_slug = models.SlugField(unique=True, null=True, blank=True)
     entry_date = models.DateTimeField(auto_now_add=True)
     entry_author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
